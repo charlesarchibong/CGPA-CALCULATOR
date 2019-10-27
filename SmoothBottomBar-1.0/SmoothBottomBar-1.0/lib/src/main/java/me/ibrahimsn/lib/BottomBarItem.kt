be06bb1c97c2183e6f592f7c77cf64a12f327ea0
Charles Archibong <charlesarchibong10@gmail.com>
2019-10-27 23:50:31 +0100

    Unwanted files deleted

--- SmoothBottomBar-1.0/SmoothBottomBar-1.0/lib/src/main/java/me/ibrahimsn/lib/BottomBarItem.kt
+++
@@ -1,6 +0,0 @@
-package me.ibrahimsn.lib
-
-import android.graphics.RectF
-import android.graphics.drawable.Drawable
-
-data class BottomBarItem(var title: String, val icon: Drawable, var rect: RectF = RectF())
